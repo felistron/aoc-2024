9d34163e70012ed330f595281c8d885a4e473f95
Fernando Espinosa <jferespinosa18@gmail.com>
2025-01-14 23:23:27 -0600

    Day 3 - Part 1

--- src/main.rs
+++ src/main.rs
@@ -122,6 +122,124 @@ mod day_2 {
     }
 }
 
+mod day_3 {
+    use std::str::FromStr;
+
+    struct Input {
+        string: Vec<char>,
+        current_index: usize,
+    }
+
+    impl Input {
+        fn new(input: &str) -> Input {
+            Input {
+                string: input.chars().collect(),
+                current_index: 0
+            }
+        }
+
+        fn next(&mut self) -> Option<char> {
+            if self.has_next() {
+                let c = self.string[self.current_index];
+                self.current_index += 1;
+                Some(c)
+            } else {
+                None
+            }
+        }
+
+        fn next_number(&mut self) -> Option<i32> {
+            let mut buffer = String::new();
+
+            while self.string[self.current_index].is_digit(10) {
+                buffer.push(self.string[self.current_index]);
+                self.current_index += 1;
+            }
+
+            match i32::from_str(&buffer) {
+                Ok(number) => Some(number),
+                Err(_) => None
+            }
+        }
+
+        fn has_next(&self) -> bool {
+            self.current_index + 1 < self.string.len()
+        }
+
+        fn expect(&mut self, c: char) -> bool {
+            if self.string[self.current_index] == c {
+                self.current_index += 1;
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    pub fn run(input: &str) {
+        println!("======== Day 3 ========");
+
+        let mut data = Input::new(input);
+        let mut result: i32 = 0;
+        let mut enabled_result: i32 = 0;
+        let mut instructions_enabled: bool = true;
+
+        while data.has_next() {
+            (|| {
+                if !data.expect('d') { return; }
+                if !data.expect('o') { return; }
+
+                if data.expect('(') {
+                    if !data.expect(')') { return; }
+                    instructions_enabled = true;
+                } else {
+                    if !data.expect('n') { return; }
+                    if !data.expect('\'') { return; }
+                    if !data.expect('t') { return; }
+                    if !data.expect('(') { return; }
+                    if !data.expect(')') { return; }
+
+                    instructions_enabled = false;
+                }
+            })();
+
+            (|| {
+                if !data.expect('m') { return; }
+                if !data.expect('u') { return; }
+                if !data.expect('l') { return; }
+                if !data.expect('(') { return; }
+
+                let a = match data.next_number() {
+                    Some(number) => number,
+                    None => return
+                };
+
+                if !data.expect(',') { return; }
+
+                let b = match data.next_number() {
+                    Some(number) => number,
+                    None => return
+                };
+
+                if !data.expect(')') { return; }
+
+                if instructions_enabled {
+                    enabled_result += a * b;
+                }
+
+                result += a * b;
+            })();
+
+            data.next();
+        }
+
+        println!("Part 1: {}", result);
+        // TODO: Part 2 works just fine with the example but it doesn't with the input.
+        // I'll come back to fix it some day :)
+        // println!("Part 2: {}", enabled_result);
+    }
+}
+
 fn print_aoc() {
     println!("~~~~~~~~~~~~~~~~~~~~~~~");
     println!("# Advent of code 2024 #");
@@ -158,4 +276,5 @@ fn main() {
 
     day_1::run(include_str!("../assets/input_d1.txt"));
     day_2::run(include_str!("../assets/input_d2.txt"));
+    day_3::run(include_str!("../assets/input_d3.txt"));
 }
